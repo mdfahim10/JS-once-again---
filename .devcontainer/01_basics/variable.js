ab61ae4ed9de873fa95193ec78f4ac76f9718c3f
MD FAHIM <217421678+mdfahim10@users.noreply.github.com>
2026-07-22 09:47:33 +0000

    learned variables

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "fahim@gmail.com"
+var accountPassword = "4567890-"
+accountCity = "Kolkata"
+
+accountEmail = "Alex@gmail.com"
+accountPassword = "987654"
+accountCity = "Patna"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity])
