002dfcc3ba6d9e754485c663f98d1966efb3e840
MD FAHIM <217421678+mdfahim10@users.noreply.github.com>
2026-07-24 21:59:03 +0530

    Stack and Heap Memory in Javascript

---
+++ .devcontainer/01_basics/04_Stack_Heap.js
@@ -0,0 +1,17 @@
+let myName="Fahim"
+let anotherName = myName
+anotherName="Alex"
+
+console.log(myName)
+console.log(anotherName)
+
+let user1={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+let user2=user1
+
+user2.email="alex@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
