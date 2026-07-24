3be8b25b25ddb1b00dcf7c30b8f4b971c92caa3f
MD FAHIM <217421678+mdfahim10@users.noreply.github.com>
2026-07-24 22:19:24 +0530

    basic strings done

---
+++ .devcontainer/01_basics/05_Strings.js
@@ -0,0 +1,9 @@
+const name="Alex"
+const repoCount=10
+
+console.log(`Hello my name is ${name} and my repository count is ${repoCount}`)
+
+const myName="Fahim"
+console.log(myName.length)
+console.log(myName.toUpperCase())
+console.log(myName.charAt(2))
