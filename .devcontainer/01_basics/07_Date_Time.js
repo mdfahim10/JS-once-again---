90605b8dbeb1cb69e472b62d8d0d254d55a9bfca
MD FAHIM <217421678+mdfahim10@users.noreply.github.com>
2026-07-25 12:45:59 +0530

    Date and Time in JS

---
+++ .devcontainer/01_basics/07_Date_Time.js
@@ -0,0 +1,21 @@
+let mydate = new Date();
+
+// console.log(mydate);
+// console.log();
+// console.log(mydate.toString());
+// console.log();
+// console.log(mydate.toDateString());
+// console.log();
+// console.log(mydate.toLocaleString());
+// console.log();
+// console.log(mydate.toLocaleTimeString());
+// console.log();
+// console.log(typeof mydate);
+
+let myCreatedDate= new Date(2012, 0, 23);
+
+let myAnotherDate=new Date(2017, 3, 21, 19, 30);
+
+console.log(myCreatedDate.toDateString());
+
+console.log(myAnotherDate.toString());
